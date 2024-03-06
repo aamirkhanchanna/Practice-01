fa9dcfa939f683e75527ae103238864d139f913d
Aamir Khan Channa <160345903+aamirkhanchanna@users.noreply.github.com>
2024-03-06 22:48:05 +0500

    Add files via upload

---
+++ note.ts
@@ -0,0 +1,30 @@
+let Dua = "In the name of Allah, The Most Gracious and The Most Mercifulِ";
+console.log(Dua)
+let myName: string = "Aamir Khan Channa";
+console.log(myName)
+let myAge: number = 25
+console.log(myAge)
+let favouriteColor = "Black";
+console.log(favouriteColor)
+favouriteColor = "White";
+console.log(favouriteColor)
+let married: boolean = true;
+console.log(married)
+let learning: any = "https://linktr.ee/giaic";
+console.log(learning)
+let faceBook: any = "https://www.facebook.com/aamirkhanchannaa/";
+console.log(faceBook)
+let instagram: any = "https://www.instagram.com/aamirkhanchanna/";
+console.log(instagram)
+let twitter: any = "https://twitter.com/aamirkhanchanna/";
+console.log(twitter)
+let linkdenIn: any = "https://www.linkedin.com/in/aamirkhanchanna/";
+console.log(linkdenIn)
+let gitHub: any = "https://github.com/aamirkhanchanna/";
+console.log(gitHub)
+let discord: any = "https://discord.com/channels/@meaamirkhanchanna";
+console.log(discord)
+let email: any = "aamirkhanchanna@yahoo.com";
+console.log(email)
+let cellNumber: any = "+923005044416";
+console.log(cellNumber)
